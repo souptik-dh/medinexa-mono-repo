20a0b69b12e4b5aa90c8acb6ec3251fd3019910e
fl-souptik-dhar <souptik.dhar@flexiloans.com>
2026-08-11 13:03:38 +0530

    update

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const allowedOrigins = ["http://localhost:3000","https://medinexa-clinic.onrender.com"];
+const allowedOrigins = ["http://localhost:3000","https://medinexa-clinic.onrender.com","http://localhost:8081"];
 
 const corsOptions = {
   "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
